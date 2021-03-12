80eb49993a327f36a57ecf1b635ef71146e2ffc4
piotrromatowski <59802218+piotrromatowski@users.noreply.github.com>
2021-03-12 20:50:59 +0100

    css for car rental app

---
+++ car rental app/main.js
@@ -0,0 +1,6 @@
+// const btn = document.querySelector(".burger");
+const btn = document.getElementById("burger");
+
+btn.addEventListener("click", () => {
+  btn.classList.toggle("burger-active");
+});
