3cd383b59f31189b52ef2dd15a04ea94e00719c5
piotrromatowski <59802218+piotrromatowski@users.noreply.github.com>
2021-10-19 18:14:50 +0200

    CRA

---
+++ Vet-Clinic-React/vet-clinic/src/App.js
@@ -0,0 +1,13 @@
+import "./styles/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>siema</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
